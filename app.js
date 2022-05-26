a378ba72cbe959cc359f1995bd5a4c03d5e53a55
Fation <fationcuro06@gmail.com>
2022-05-26 18:05:15 +0200

    cooding

---
+++ app.js
@@ -0,0 +1,15 @@
+let job ={
+    title:'Developer',
+    place:'Albania',
+    salary:'50000',
+};
+
+
+let adultYears = age - 16;
+alert(adultYears);
+
+age = 45;
+adultYears = age - 18;
+
+
+alert(adultYears);

fa9b3b82bfa0dfaa0321e17b6d2564af52989bf7
Fation <fationcuro06@gmail.com>
2022-05-29 12:30:03 +0200

    fix code

---
+++ app.js
@@ -0,0 +1,17 @@
+// var moshaJote = 16;   //numer
+// var emriYt = "Arsen";   //string
+// var emri = {emri: "Fation", mbiemri: "Curo" };   //objekti
+// var truth = false;  //boolean
+// var fruta = ["molle", "portokall", "banane"];  //matrica
+// var random; //undefined
+
+var fruti = 'banane';
+var frutaTeTjera = 'banane/nmolle';
+console.log(fruti.length);
+console.log(fruti.indexOf('an'));
+console.log(fruti.slice(2, 6));
+console.log(fruti.replace('ban', '123'));
+console.log(fruti.toUpperCase());
+console.log(fruti,toLowerCase());
+console.log(fruti[2]);
+console.log(fruti.split(''));

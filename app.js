ff54c1f0e228ff0e1706b90ad281fcefec71de7d
Fation <fationcuro06@gmail.com>
2022-05-29 14:53:40 +0200

    cooding with js

--- app.js
+++ app.js
@@ -5,13 +5,22 @@
 // var fruta = ["molle", "portokall", "banane"];  //matrica
 // var random; //undefined
 
-var fruti = 'banane';
-var frutaTeTjera = 'banane/nmolle';
-console.log(fruti.length);
-console.log(fruti.indexOf('an'));
-console.log(fruti.slice(2, 6));
-console.log(fruti.replace('ban', '123'));
-console.log(fruti.toUpperCase());
-console.log(fruti,toLowerCase());
-console.log(fruti[2]);
-console.log(fruti.split(''));
+// var fruti = 'banane';
+// var frutaTeTjera = 'banane/nmolle';
+// console.log(fruti.length);
+// console.log(fruti.indexOf('an'));
+// console.log(fruti.slice(2, 6));
+// console.log(fruti.replace('ban', '123'));
+// console.log(fruti.toUpperCase());
+// console.log(fruti.toLowerCase());
+// console.log(fruti[2]);
+// console.log(fruti.split(''));
+
+var frutat = ['banane', 'molle', 'portokall', 'ananas'];
+console.log(frutat[2]);
+frutat[0] = 'dardhe';
+console.log(frutat);
+
+for (var i = 0; i<frutat.length; i++){
+    console.log(frutat[i]);
+}

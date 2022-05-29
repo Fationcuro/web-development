e01e9652f28e60e519af17f63e1c35b2744b8bec
Fation <fationcuro06@gmail.com>
2022-05-29 10:54:51 +0200

    cooding and today

--- app.js
+++ app.js
@@ -8,6 +8,19 @@ prova();
 function greeting(){
     var name = prompt('Si e ke emrin?');
     var result = 'Pershendetje' + ' ' + name;
-    console.log_(result)
+    console.log(result)
 }
-greeting();
+// greeting();
+
+function shumaeNumrave(n1, n2){
+    var result = n1 + n2;
+    console.log(result);
+}
+
+shumaeNumrave(50, 40);
+
+var num = 0;
+while(num<100){
+    num +=1;
+    console.log(num)
+}

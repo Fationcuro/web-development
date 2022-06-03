ac74f1a674c94d7b564a5b15244da7961fe1a7f4
Fation <fationcuro06@gmail.com>
2022-06-03 11:30:33 +0200

    cooding

---
+++ app.js
@@ -0,0 +1,11 @@
+let age = 32;
+let userName = 'Fation';
+let hobbies = ['Sport', 'Cooking', 'Reading'];
+let job = {
+    title:'Deeveloper',
+    place:'New York',
+    salary: 50000,
+};
+
+alert(hobbies[0]);
+alert(job.place);

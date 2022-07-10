8a6fde409f08293c329bb0d2cdee59b51ed9bf0c
Fation <fationcuro06@gmail.com>
2022-07-10 11:51:59 +0200

    started cooding

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-let age = 32;
-let userName = 'Fation';
-let hobbies = ['Sports','Cooking', 'Reading' ];
-let job ={
-    title:'Developer',
-    place:'Albania',
-    salary:50000,
-};
-let adultYears;
+// let age = 32;
+// let userName = 'Fation';
+// let hobbies = ['Sports','Cooking', 'Reading' ];
+// let job ={
+//     title:'Developer',
+//     place:'Albania',
+//     salary:50000,
+// };
+// let adultYears;
 
-function calculateAdultYears() {
-   let adultYears = age - 16;
-}
+// function calculateAdultYears() {
+//    let adultYears = age - 16;
+// }
 
-calculateAdultYears();
-alert(adultYears);
+// calculateAdultYears();
+// alert(adultYears);
 
-age = 45;
-calculateAdultYears();
+// age = 45;
+// calculateAdultYears();
 
-alert(adultYears);
+// alert(adultYears);
 
